174cb7c30d26fa02815febda19b78cd6e58907fa
Nadezhda <nadyakj00@gmail.com>
2022-02-10 22:24:13 +0900

    Add Task 5

---
+++ Project_Khovanets/src/test/java/online_tusur/unit_online_tusur/StudentTestNG.java
@@ -0,0 +1,24 @@
+package online_tusur.unit_online_tusur;
+
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class StudentTestNG {
+	@Test
+	@Parameters({ "expectedAverAge", "students" })
+	public void TestNG_AgeAvg(@Optional("25") int expectedAverAge, @Optional("Иван Иванов 20,Семен Семенов 30") String students) {
+		String[] studentArr = students.split(",");
+		Student[] arrOfStudent = new Student[studentArr.length];
+
+		for (int i = 0; i < studentArr.length; i++) {
+			arrOfStudent[i] = new Student();
+			String[] studInf = studentArr[i].split(" ");
+			
+			arrOfStudent[i].setFirstName(studInf[0]);
+			arrOfStudent[i].setLastName(studInf[1]);
+			arrOfStudent[i].setAge(Integer.parseInt(studInf[2]));
+		}
+		assert expectedAverAge == Student.avgAge(arrOfStudent);
+	}
+}
